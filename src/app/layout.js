affce1fe4e27596fc0fe58bd228f0adfed355419
harshsindhu0408 <harshsindhu0402@gmail.com>
2025-12-04 15:00:27 +0530

    feat: integrate Google Tag Manager scripts for analytics tracking

--- src/app/layout.js
+++ src/app/layout.js
@@ -108,6 +108,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+                  __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PZBBS2NH');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
         {/* Add JSON-LD to the head */}
         <script
           type="application/ld+json"
@@ -117,6 +128,16 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PZBBS2NH"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <NavigationWrapper />
         {children}
         <Footer />
